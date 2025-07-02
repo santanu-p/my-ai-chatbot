104e2bbab180a220c8ecc0e411a1e4ea99b1292d
Santanu Pradhan <psantanu293@gmail.com>
2025-07-02 11:27:18 +0530

    Update app-sidebar.tsx

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -25,7 +25,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                     <MessagesSquare className="size-4" />
                   </div>
                   <div className="flex flex-col gap-0.5 leading-none">
-                    <span className="font-semibold">Nexis</span>
+                    <span className="font-semibold">Nexis Gold</span>
                   </div>
                 </Link>
               </SidebarMenuButton>
